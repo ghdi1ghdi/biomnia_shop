7ea458a3e763d3426be317760bb3d7a2d1883dcf
Lee Jeong Ho <73730434+ghdi1ghdi@users.noreply.github.com>
2023-07-23 20:57:05 +0900

    필요없는 주석 삭제

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -1,7 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { faker } from "@faker-js/faker";
 // @mui
-// import { useTheme } from "@mui/material/styles";
 import { styled } from "@mui/material/styles";
 import { Grid, Container, Typography } from "@mui/material";
 // components
