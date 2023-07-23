e033b31a3fad224686706994e4095bc58bc320cd
Lee Jeong Ho <73730434+ghdi1ghdi@users.noreply.github.com>
2023-07-23 21:42:57 +0900

    좀 이젠 되라

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -1,10 +1,10 @@
 import { Helmet } from "react-helmet-async";
 import { faker } from "@faker-js/faker";
 // @mui
-import { useTheme } from "@mui/material/styles";
+// import { useTheme } from "@mui/material/styles";
 import { Grid, Container, Typography } from "@mui/material";
 // components
-import Iconify from "../components/iconify";
+// import Iconify from "../components/iconify";
 // sections
 import {
   AppTasks,
@@ -20,7 +20,7 @@ import {
 
 // ----------------------------------------------------------------------
 export default function DashboardAppPage() {
-  const theme = useTheme();
+  // const theme = useTheme();
   return (
     <>
       <Helmet>
